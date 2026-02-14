be4d510ff9a5dabadfc1b1462cc6d4e04d255f15
Siam-moni <shariars012@gmail.com>
2026-02-14 12:43:28 +0600

    task-6

---
+++ task-6.js
@@ -0,0 +1,8 @@
+const students = [
+  { name: "John", marks: 85 },
+  { name: "Alice", marks: 90 },
+];
+for (let i = 0; i < students.length; i++) {
+  let student = students[i];
+  console.log(student.name, student.marks);
+}
